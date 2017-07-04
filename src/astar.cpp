8f83cbfc3b7709826a9bbbf4db442cd850bfe3f0
Florian Klemme <mail@florianklemme.de>
2017-07-04 04:20:06 +0200

    Implemented own priority queue to provide "find" function.

--- src/astar.cpp
+++ src/astar.cpp
@@ -1,45 +1,124 @@
 #include "astar.h"
 
-#include <queue>
-#include <set>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+namespace {
+struct NodeCost {
+    NodeCost(Node _node, float _cost, float _heuristic, Node _predecessor)
+        : node(std::move(_node)), cost(_cost), heuristic(_heuristic),
+          predecessor(std::move(_predecessor)) {}
+
+    Node  node;
+    float cost;        // g-value
+    float heuristic;   // h-value
+    Node  predecessor; // to recreate path
+};
+
+// Comparator to put cheapest nodes first.
+bool compare(const NodeCost &a, const NodeCost &b) {
+    return a.cost + a.heuristic > b.cost + b.heuristic;
+};
+
+class NodePrioQueue {
+public:
+    NodeCost top() const { return m_heap.front(); }
+    bool     empty() const { return m_heap.empty(); }
+    auto     size() const { return m_heap.size(); }
+
+    void push(const NodeCost &nc) {
+        m_heap.push_back(nc);
+        std::push_heap(m_heap.begin(), m_heap.end(), compare);
+    }
+
+    template <typename... Args>
+    void emplace(Args &&... args) {
+        m_heap.emplace_back(std::forward<Args>(args)...);
+        std::push_heap(m_heap.begin(), m_heap.end(), compare);
+    }
+
+    NodeCost pop() {
+        std::pop_heap(m_heap.begin(), m_heap.end(), compare);
+        auto result = std::move(m_heap.back());
+        m_heap.pop_back();
+        return result;
+    }
+
+    // This member function doesn't exist in std::priority_queue.
+    template <typename Function>
+    bool findNodeAndApply(const Node &node, Function f) {
+        int index = findNode(node);
+
+        if (index < (int) m_heap.size()) {
+            auto old = m_heap[index];
+            f(m_heap[index]);
+
+            // If node has changed, update heap.
+            if (compare(m_heap[index], old) == compare(old, m_heap[index])) {
+                std::make_heap(m_heap.begin(), m_heap.end(), compare); // probably suboptimal
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+private:
+    int findNode(const Node &node, int index = 0) const {
+        if (index >= (int) m_heap.size())
+            return m_heap.size();
+
+        if (m_heap[index].node == node)
+            return index;
+
+        return std::min(findNode(node, index * 2 + 1), findNode(node, index * 2 + 2));
+    }
+
+    std::vector<NodeCost> m_heap;
+};
+}
 
 std::vector<Node> cpuAStar(const Graph &g, const Position &source, const Position &destination) {
-    using NodeCost = std::pair<Node, float>;
-    auto compare = [](const NodeCost &a, const NodeCost &b) { return a.second > b.second; };
 
     // Open and closed list
-    std::priority_queue<NodeCost, std::vector<NodeCost>, decltype(compare)> open(compare);
-    std::set<Node>    closed;
-    std::vector<Node> path;
+    NodePrioQueue open;
+    std::map<Node, Node> closed; // store predecessor to recreate path
 
     // Begin at source
-    open.emplace(Node(g, source), 0.0f);
+    const Node sourceNode(g, source);
+    open.emplace(sourceNode, 0.0f, 0.0f, sourceNode);
 
     while (!open.empty()) {
-        const auto node = open.top().first;
-        const auto cost = open.top().second;
+        const auto current = open.top();
         open.pop();
 
-        // There may be duplicates, check if already visited
-        if (closed.count(node) == 1)
-            continue;
-
-        // Remember path
-        path.push_back(node);
-        closed.insert(node);
-
-        if (node.position() == destination) {
-            return path;
+        if (current.node.position() == destination) {
+            return {}; // TODO
         }
 
-        // Expand node
-        for (const auto &neighbor : node.neighbors()) {
-            const auto &nbNode = neighbor.first;
-            const auto &nbCost = neighbor.second;
+        closed.emplace(current.node, current.predecessor);
 
-            if (closed.count(nbNode) == 0) {
-                const auto totalCost = cost + nbCost + (destination - nbNode.position()).length();
-                open.emplace(nbNode, totalCost);
+        // Expand node
+        for (const auto &neighbor : current.node.neighbors()) {
+            const auto &neighborNode = neighbor.first;
+            const auto &neighborCost = neighbor.second;
+
+            if (closed.count(neighborNode) == 0) {
+                const auto cost = current.cost + neighborCost;
+                const auto heuristic = (destination - neighborNode.position()).length();
+
+                bool nodeAlreadyInOpenList = open.findNodeAndApply(neighborNode, [&](NodeCost &nc) {
+                    if (cost < nc.cost) {
+                        // Found a cheaper path. Update node.
+                        nc.cost = cost;
+                        nc.predecessor = current.node;
+                    }
+                });
+
+                if (!nodeAlreadyInOpenList) {
+                    open.emplace(neighborNode, cost, heuristic, current.node);
+                }
             }
         }
     }
