83e4b1ce79bd644b2e9ad10d769c8a658eac751d
Florian Klemme <kruecke@kruecke.net>
2017-07-22 01:19:10 +0200

    Run a bunch of path finds.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "Graph.h"
 #include "astar.h"
 #include <algorithm>
+#include <cassert>
 #include <iostream>
+#include <random>
+#include <vector>
 
 static float costs(const std::vector<Node> &path) {
     if (path.empty())
@@ -19,34 +22,55 @@ static float costs(const std::vector<Node> &path) {
 
 int main() {
     // Generate graph and obstacles
-    Graph graph(200, 200);
+    Graph graph(100, 100);
     graph.generateObstacles();
 
-    const Position start{10, 20};
-    const Position destination{180, 190};
+    // Generate source/destination pairs
+    const int pathCount = 100;
 
-    // CPU (reference) run
-    const auto cpuPath = cpuAStar(graph, start, destination);
-    graph.toPfm("cpuAStar.pfm", cpuPath);
+    std::random_device                 rd;
+    std::default_random_engine         generator(rd());
+    std::uniform_int_distribution<int> distX(0, graph.width() - 1);
+    std::uniform_int_distribution<int> distY(0, graph.height() - 1);
+
+    std::vector<std::pair<Position, Position>> srcDstList;
+    for (int i = 0; i < pathCount; ++i)
+        srcDstList.emplace_back(Position{distX(generator), distY(generator)},
+                                Position{distX(generator), distY(generator)});
+
+    // CPU (reference) runs
+    std::vector<std::vector<Node>> cpuPaths;
+    for (const auto &srcDst : srcDstList)
+        cpuPaths.emplace_back(cpuAStar(graph, srcDst.first, srcDst.second));
+
+    // Print graph (with first path) to image
+    graph.toPfm("cpuAStar.pfm", cpuPaths.front());
 
     try {
-        // GPU run
-        const auto  gpuPaths = gpuAStar(graph, {{start, destination}});
-        const auto &gpuPath = gpuPaths.front(); // path from first agent
+        // GPU runs
+        const auto gpuPaths = gpuAStar(graph, srcDstList);
 
-        if (std::equal(cpuPath.begin(), cpuPath.end(), gpuPath.begin(), gpuPath.end())) {
-            std::cout << "GPU A*: Gold test passed! (exact match)" << std::endl;
-        } else if (cpuPath.size() == gpuPath.size() && std::abs(costs(cpuPath) - costs(gpuPath)) < 0.1f) {
-            std::cout << "GPU A*: Gold test passed! (equal match)" << std::endl;
-        } else {
-            std::cerr << "GPU A*: Gold test failed!"
-                      << "\nPath length CPU: " << cpuPath.size() << ", GPU: " << gpuPath.size()
-                      << "\nPath cost CPU: " << costs(cpuPath) << ", GPU: " << costs(gpuPath)
-                      << std::endl;
+        assert(cpuPaths.size() == gpuPaths.size());
+
+        for (std::size_t i = 0; i < cpuPaths.size(); ++i) {
+            const auto &cpuPath = cpuPaths[i];
+            const auto &gpuPath = gpuPaths[i];
+
+            if (std::equal(cpuPath.begin(), cpuPath.end(), gpuPath.begin(), gpuPath.end())) {
+                std::cout << "GPU A* " << i << ": Gold test passed! (exact match)" << std::endl;
+            } else if (cpuPath.size() == gpuPath.size() &&
+                       std::abs(costs(cpuPath) - costs(gpuPath)) < 0.1f) {
+                std::cout << "GPU A* " << i << ": Gold test passed! (equal match)" << std::endl;
+            } else {
+                std::cerr << "GPU A* " << i << ": Gold test failed!"
+                          << "\n - Path length CPU: " << cpuPath.size()
+                          << ", GPU: " << gpuPath.size() << "\n - Path cost CPU: " << costs(cpuPath)
+                          << ", GPU: " << costs(gpuPath) << std::endl;
+            }
         }
 
-        // Print graph to image
-        graph.toPfm("gpuAStar.pfm", gpuPath);
+        // Print graph (with first path) to image
+        graph.toPfm("gpuAStar.pfm", gpuPaths.front());
     } catch (std::exception &e) {
         std::cerr << "A* execution failed:\n" << e.what() << std::endl;
     }
